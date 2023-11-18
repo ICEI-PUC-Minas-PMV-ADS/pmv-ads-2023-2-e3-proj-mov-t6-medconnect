48ffae0e2f68a842c5c5062a6d5f9c88773375f8
NicolasTeixeiraa <103547208+NicolasTeixeiraa@users.noreply.github.com>
2023-11-18 14:45:36 -0300

    RecoverPassword
    Tela de recuperação de senha.

--- src/medconnect-expo/src/navigation/AuthNavigation.tsx
+++ src/medconnect-expo/src/navigation/AuthNavigation.tsx
@@ -1,5 +1,5 @@
 import { createStackNavigator} from  "@react-navigation/stack"
-import { HomeScreen, LoginScreen } from "../screens"
+import { HomeScreen, LoginScreen, RecoverPasswordScreen } from "../screens"
 import CadastroScreen from "../screens/Register/components/CadastroScreen"
 
 const Stack = createStackNavigator()
@@ -10,6 +10,7 @@ export const AuthNavigation = () => {
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Register" component={CadastroScreen} />
+            <Stack.Screen name="RecoverPassword" component={RecoverPasswordScreen} />
         </Stack.Navigator>
     )
 }
